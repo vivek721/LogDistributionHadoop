ae430639ac494253d96cb011396ed1e8e6ef9515
Vivek Mishra <vmishr8@uic.edu>
2021-10-19 01:58:21 -0500

    added jar

--- src/main/scala/ExecutionStart.scala
+++ src/main/scala/ExecutionStart.scala
@@ -1,4 +1,4 @@
-import LogMethods.{DistributionPattern, LargestMessage, MostErrorInTime, Test, TimeIntervalLogs}
+import LogMethods.{DistributionPattern, LargestMessage, MostErrorInTime, TimeIntervalLogs}
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
